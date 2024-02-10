c85eb85be45ed58439b268299f043cdffb0d6f51
Hassan Khaled <58123330+HassanKha@users.noreply.github.com>
2024-02-10 21:08:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(cors({
 dotenv.config();
 app.use(express.json());
 const _dirname = path.resolve();
+app.use(express.static(__dirname + "/public"));
 //app.use(express.static(path.join(_dirname,'/client/my-project/dist')));
 const upload = multer({ dest: "uploads/" });
 
